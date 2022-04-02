0768173a412a2ad4ede4dce5726ece2d9ebbb213
Claire Chiou <clairefantastic7@gmail.com>
2022-04-02 09:43:23 +0800

    [feat] Import Firebase through Cocoapods

---
+++ Publisher/Publisher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Publisher
+//
+//  Created by claire on 2022/4/2.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
