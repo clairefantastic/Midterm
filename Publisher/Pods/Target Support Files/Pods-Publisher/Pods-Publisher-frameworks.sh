9e221f356b642580bdb94f7b18c798027faa80e3
Claire Chiou <clairefantastic7@gmail.com>
2022-04-02 10:32:35 +0800

    [feat] Add MJRefresh

--- Publisher/Pods/Target Support Files/Pods-Publisher/Pods-Publisher-frameworks.sh
+++ Publisher/Pods/Target Support Files/Pods-Publisher/Pods-Publisher-frameworks.sh
@@ -178,6 +178,7 @@ code_sign_if_enabled() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/BoringSSL-GRPC/openssl_grpc.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromisesObjC/FBLPromises.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/abseil/absl.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/gRPC-C++/grpcpp.framework"
@@ -188,6 +189,7 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/BoringSSL-GRPC/openssl_grpc.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromisesObjC/FBLPromises.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/abseil/absl.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/gRPC-C++/grpcpp.framework"
--- Publisher/Publisher/Controller/ViewController.swift
+++ Publisher/Publisher/Controller/ViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import Firebase
+import MJRefresh
 
 class ViewController: UIViewController {
 
