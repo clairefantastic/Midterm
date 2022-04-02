67c783a06eecfd24941564d5a74b20e272d555f3
Claire Chiou <clairefantastic7@gmail.com>
2022-04-02 17:33:37 +0800

    [fix] Adjust lines

--- Publisher/Publisher/Model/FirebaseManager.swift
+++ Publisher/Publisher/Model/FirebaseManager.swift
@@ -11,21 +11,35 @@ import Firebase
 class FirebaseManager {
     
     func addData(_ article: PublishedArticle) {
+        
         let articles = Firestore.firestore().collection("articles")
+        
         let document = articles.document()
+        
         let data: [String: Any] = [
+            
             "author": [
+                
                 "email": "\(article.author.authorEmail)",
+                
                 "id": "\(article.author.authorId)",
+                
                 "name": "\(article.author.authorName)"
+                
             ],
+            
             "title": "\(article.articleTitle)",
+            
             "content": "\(article.articleContent)",
-    "createdTime": Date().timeIntervalSince1970,
-    "id": document.documentID,
-    "category": "\(article.articleCategory)"
-    ]
-    document.setData(data)
+            
+            "createdTime": Date().timeIntervalSince1970,
+            
+            "id": document.documentID,
+            
+            "category": "\(article.articleCategory)"
+        ]
+        
+        document.setData(data)
     }
 }
 
